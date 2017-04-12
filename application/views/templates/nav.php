72076a04dcddc4d6c4e7a6ebece9a3009d78aba9
Daniel Cataño <dasniel19910@gmail.com>
2017-04-12 11:07:36 -0500

    El logo redirecciona a la página de cursos

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -1,6 +1,6 @@
 <header>
     <nav>
-        <a class="valign-wrapper" href=""><h1 class="valign logo">MathMaster</h1></a>
+        <a class="valign-wrapper" href="<?= base_url() ?>/index.php/Curso/cargar_lista_cursos"><h1 class="valign logo">MathMaster</h1></a>
         <div>
             <ul>
                 <li><a href="" id="tabla_puntuaciones">Tabla de puntuaciones</a></li>
