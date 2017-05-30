a531d92a0b9aa9ca043942d6042054e5a696e49d
Daniel Cataño <dasniel19910@gmail.com>
2017-05-30 11:45:12 -0500

    La explicación se ve visualmente mejor

--- application/views/explicacion.php
+++ application/views/explicacion.php
@@ -4,5 +4,5 @@
         <a href="<?= base_url() ?>index.php/pregunta/iniciar_test/<?= $id ?>" class="waves-effect waves-light btn blue">Comenzar el test</a>
     </div>
     <div class="divider m20-0"></div>
-    <p><?= $explicacion ?></p>
+    <textarea class="materialize-textarea" disabled><?= $explicacion ?></textarea>
 </div>
